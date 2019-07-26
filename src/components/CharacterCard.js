09de3aff8321b5f08dea46895149a39790593e25
lilyhoratio <lilyhzhou@gmail.com>
2019-07-26 09:02:59 -0700

    mapped over characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard ({character}) {
+  const {id, name, status, species, location} = character;
+  console.log(location)
+  return (
+    <div>
+      <h2>{name}</h2>
+      <span>{location.name}</span>
+    </div>
+  )
 }
