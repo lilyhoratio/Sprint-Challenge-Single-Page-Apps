0b349d7e565d3ca452d3f50786caff80666dc656
lilyhoratio <lilyhzhou@gmail.com>
2019-07-26 09:18:16 -0700

    semantic ui for character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {Card, Image, Header} from 'semantic-ui-react';
 
 export default function CharacterCard ({character}) {
-  const {id, name, status, species, location} = character;
-  console.log(location)
+  const {id, name, status, species, location, image} = character;
+  
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{location.name}</span>
-    </div>
+    <Card>
+      <Image src={image} />
+      <Card.Header>{name}</Card.Header>
+      <Card.Meta>{species}</Card.Meta>
+      <Card.Description>Rick is {status} and lives in {location.name}</Card.Description>
+    </Card>
   )
-}
+};
